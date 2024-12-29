8b79cfd195bd47105225af7f8fe7162ea9dddd50
somnathdas76 <57346047+somnathdas76@users.noreply.github.com>
2024-12-29 10:30:29 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'somnath das',
+    author_email= 'somnath.das76@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
